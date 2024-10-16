359fb0e6ba4fae41e1918a752f660f74113b64c9
jeonggeun.song <jeonggeun.song@42dot.ai>
2024-10-16 11:27:51 +0900

    minor improvement and affine layers added

--- tuls/data/loaders.py
+++ tuls/data/loaders.py
@@ -1,9 +1,12 @@
 import glob
+import os
 
 from tuls.data.constants import IMAGE_EXTENSIONS
 
 
 def load_all_image_paths(source):
+    if not source.endswith('{ext}'):
+        source = os.path.join(source, '*.{ext}')
     image_paths = []
     for ext in IMAGE_EXTENSIONS:
         image_paths.extend(glob.glob(source.format(ext=ext)))
---
+++ tuls/torch/layers/affine.py
@@ -0,0 +1,35 @@
+from functools import partial
+
+import torch
+from torch import nn
+
+
+class Affine(nn.Module):
+    def __init__(self, num_features, bias=True):
+        super().__init__()
+        self.num_features = num_features
+        self.weight = nn.Parameter(torch.randn(num_features))
+        if bias:
+            self.bias = nn.Parameter(torch.randn(num_features))
+        else:
+            self.bias = None
+
+    def forward(self, x, dim=None):
+        size = x.size()
+        weight = self.weight
+        bias = self.bias
+        # guess dimension to apply
+        if dim is None:
+            # iterate except batch dim
+            for index, d in enumerate(size[1:]):
+                if d == self.num_features:
+                    dim = index
+                    break
+            else:
+                raise ValueError('There is no dimension to apply Affine module.')
+        weight = weight.view(*[-1 if index == dim else 1 for index in range(len(size))])
+        bias = bias.view(*[-1 if index == dim else 1 for index in range(len(size))])
+        return weight*x+bias
+
+
+LayerScale = partial(Affine, bias=False)
