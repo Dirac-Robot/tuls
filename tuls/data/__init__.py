f99ac2cdea2a3bc27fb2636f557769262a9fa2ef
jeonggeun.song <jeonggeun.song@42dot.ai>
2024-10-21 19:51:46 +0900

    split_into_batches added

--- tuls/data/__init__.py
+++ tuls/data/__init__.py
@@ -1,4 +1,4 @@
-from typing import Sequence, Mapping, Union
+from typing import Sequence, Mapping, Union, overload
 
 
 def gather_by_keys(items: Sequence[Mapping], keys: Union[str, Sequence[str]]):
@@ -26,3 +26,28 @@ def permute_mappings(items: Union[Sequence[Mapping], Mapping[str, Sequence]]):
         return scatter_by_keys(items, list(items.keys()))
     else:
         raise TypeError('items should be either Sequence of Mappings or Mapping of Sequences.')
+
+
+@overload
+def split_into_batches(items, batch_size, cut_tail):
+    pass
+
+
+@overload
+def split_into_batches(items, num_batches, cut_tail):
+    pass
+
+
+def split_into_batches(items, batch_size=None, num_batches=None, cut_tail=True):
+    if batch_size is not None:
+        batches = [items[index:index+batch_size] for index in range(0, len(items), batch_size)]
+        if cut_tail and len(batches[-1]) < batch_size:
+            batches.pop(-1)
+    elif num_batches is not None:
+        batch_size = len(items)//num_batches
+        if not cut_tail:
+            batch_size += 1
+        batches = [items[index:index+batch_size] for index in range(0, len(items), batch_size)]
+    else:
+        raise ValueError('At least one of batch_size or num_batches must be specified.')
+    return batches
