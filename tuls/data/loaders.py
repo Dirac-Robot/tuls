1f78fd682003751afb214117711fef4a4fb12dd5
Dirac <tatari_@naver.com>
2024-11-07 13:28:11 +0900

    video loading

--- tuls/data/loaders.py
+++ tuls/data/loaders.py
@@ -1,11 +1,9 @@
 import glob
 import os
-from pathlib import Path
-
-from parse import *
+import cv2
+from typing import overload
 
 from tuls.data.constants import IMAGE_EXTENSIONS
-from tuls.misc.sequence import ensure_sequence
 
 
 def load_all_image_paths(source):
@@ -17,40 +15,32 @@ def load_all_image_paths(source):
     return image_paths
 
 
-class PathFinder:
-    @classmethod
-    def get_file_name(cls, path, ext=None):
-        file_name = os.path.splitext(os.path.basename(path))[0]
-        if ext is not None:
-            file_name = f'{file_name}.{ext}'
-        return file_name
+@overload
+def extract_images_from_video(source, output_dir, sampling_rate):
+    pass
 
-    @classmethod
-    def split(cls, path):
-        return path.split(os.path.sep)
 
-    @classmethod
-    def join(cls, paths_1, paths_2):
-        paths_1 = [Path(path) for path in ensure_sequence(paths_1)]
-        paths_2 = [Path(path) for path in ensure_sequence(paths_2)]
-        if len(paths_1) == 1:
-            paths_1 = paths_1*len(paths_2)
-        if len(paths_2) == 1:
-            paths_2 = paths_2*len(paths_1)
-        if len(paths_1) != len(paths_2):
-            raise ValueError('The length of paths_1 and paths_2 must be 1 or same.')
-        return [str(path_1/path_2) for path_1, path_2 in zip(paths_1, paths_2)]
+@overload
+def extract_images_from_video(source, output_dir, num_frames):
+    pass
 
-    @classmethod
-    def map_by_number(cls, paths=None, path_format=None, map_fn=None):
-        if paths is None:
-            paths = glob.glob(path_format)
-        parse_format = path_format.replace('*', '{:d}')
-        numbers = [int(parse(parse_format, path)[0]) for path in paths]
-        return map_fn(numbers, paths) if map_fn is not None else numbers, paths
 
-    @classmethod
-    def map_by_time(cls, path_format, map_fn=None):
-        paths = glob.glob(path_format)
-        times = [os.path.getmtime(path) for path in paths]
-        return map_fn(times, paths) if map_fn is not None else times, paths
+def extract_images_from_video(source, output_dir, sampling_rate=None, num_frames=None):
+    os.makedirs(output_dir, exist_ok=True)
+    video = cv2.VideoCapture(source)
+    frames = []
+    frame_index = 0
+    while True:
+        result, frame = video.read()
+        if not result:
+            break
+        frames.append(dict(index=frame_index, frame=frame))
+        frame_index += 1
+    if num_frames is not None:
+        sampling_rate = len(frames)//num_frames
+    frames = [frames[frame_index] for frame_index in range(0, sampling_rate, len(frames))]
+    for frame_info in frames:
+        file_path = os.path.join(output_dir, f'frame_{frame_info["index"]:06d}.png')
+        cv2.imwrite(file_path, frame_info['frame'])
+    video.release()
+    cv2.destroyAllWindows()
